71d89026cac25226db5ca9f2211e2c5cf47ae683
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-10-11 14:26:47 +0700

    Added `keyword` & `Filter Attribute`

--- insect_dection_app/lib/features/insect/domain/usecases/params/insect_list_params.dart
+++ insect_dection_app/lib/features/insect/domain/usecases/params/insect_list_params.dart
@@ -1,20 +1,44 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:insect_dection_app/features/insect/insect.dart';
 
+enum TaxonomicRank { species, genus, family, order, classies, phylum, regnum }
+
+extension TaxonomicRankX on TaxonomicRank {
+  String get attribute {
+    switch (this) {
+      case TaxonomicRank.classies:
+        return "taxonomy.class";
+      default:
+        return "taxonomy.$name";
+    }
+  }
+}
+
+enum NomenclatureName { scientificName, otherName, commonName }
+
+extension NomenclatureNameX on NomenclatureName {
+  String get attribute => "nomenclature.$name";
+}
+
 class InsectListParams extends InsectList {
-  // TODO: add fillter
+  final String? keyword;
+  final String? filterAttribute;
+
   const InsectListParams({
     required super.hasNextPage,
     super.size = 0,
     required super.previousPage,
     required super.currentPage,
+    this.keyword,
+    this.filterAttribute ,
   });
 
-  static InsectListParams get empty => const InsectListParams(
+  static InsectListParams get empty =>  InsectListParams(
         hasNextPage: false,
-        previousPage: <Insect>[],
-        currentPage: <Insect>[],
+        previousPage: const <Insect>[],
+        currentPage: const <Insect>[],
         size: 0,
+        filterAttribute:  NomenclatureName.commonName.attribute,
       );
   @override
   InsectListParams copyWith({
@@ -22,12 +46,16 @@ class InsectListParams extends InsectList {
     int? size,
     List<Insect>? previousPage,
     List<Insect>? currentPage,
+    String? keyword,
+    String? filterAttribute,
   }) {
     return InsectListParams(
       hasNextPage: hasNextPage ?? super.hasNextPage,
       size: size ?? super.size,
       previousPage: previousPage ?? super.previousPage,
       currentPage: currentPage ?? super.currentPage,
+      keyword: keyword ?? this.keyword,
+      filterAttribute: filterAttribute ?? this.filterAttribute,
     );
   }
 
@@ -37,9 +65,17 @@ class InsectListParams extends InsectList {
       size: entity.size,
       previousPage: entity.previousPage,
       currentPage: entity.currentPage,
+      filterAttribute: NomenclatureName.commonName.attribute,
     );
   }
 
   @override
-  List<Object> get props => [hasNextPage, size, previousPage, currentPage];
+  List<Object?> get props => [
+        hasNextPage,
+        size,
+        previousPage,
+        currentPage,
+        keyword,
+        filterAttribute,
+      ];
 }
---
+++ insect_dection_app/test/features/insect/domain/usecases/params/insect_list_params_test.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:insect_dection_app/features/features.dart';
+
+
+void main() {
+  group('InsectListParams', () {
+    test('copyWith() should return a new InsectListParams instance with the updated fields', () {
+      // Arrange
+      final insectListParams = InsectListParams(
+        hasNextPage: true,
+        size: 10,
+        previousPage: [Insect.empty],
+        currentPage: [Insect.empty],
+        keyword: 'keyword',
+        filterAttribute: 'filterAttribute',
+      );
+
+      // Act
+      final updatedInsectListParams = insectListParams.copyWith(
+        hasNextPage: false,
+        size: 20,
+        previousPage: [Insect.empty, Insect.empty],
+        currentPage: [Insect.empty, Insect.empty],
+        keyword: 'updated keyword',
+        filterAttribute: 'updated filterAttribute',
+      );
+
+      // Assert
+      expect(updatedInsectListParams.hasNextPage, false);
+      expect(updatedInsectListParams.size, 20);
+      expect(updatedInsectListParams.previousPage.length, 2);
+      expect(updatedInsectListParams.currentPage.length, 2);
+      expect(updatedInsectListParams.keyword, 'updated keyword');
+      expect(updatedInsectListParams.filterAttribute, 'updated filterAttribute');
+    });
+
+    test('fromEntity() should create a InsectListParams instance from a InsectList entity', () {
+      // Arrange
+      final insectListEntity = InsectList(
+        hasNextPage: true,
+        size: 10,
+        previousPage: [Insect.empty],
+        currentPage: [Insect.empty],
+      );
+
+      // Act
+      final insectListParams = InsectListParams.fromEntity(insectListEntity);
+
+      // Assert
+      expect(insectListParams.hasNextPage, true);
+      expect(insectListParams.size, 10);
+      expect(insectListParams.previousPage.length, 1);
+      expect(insectListParams.currentPage.length, 1);
+    });
+
+    test('props should return a list of all the properties of the InsectListParams instance', () {
+      // Arrange
+      final insectListParams = InsectListParams(
+        hasNextPage: true,
+        size: 10,
+        previousPage: [Insect.empty],
+        currentPage: [Insect.empty],
+        keyword: 'keyword',
+        filterAttribute: 'filterAttribute',
+      );
+
+      // Act
+      final props = insectListParams.props;
+
+      // Assert
+      expect(props, [
+        true,
+        10,
+        [Insect.empty],
+        [Insect.empty],
+        'keyword',
+        'filterAttribute',
+      ]);
+    });
+  });
+}
