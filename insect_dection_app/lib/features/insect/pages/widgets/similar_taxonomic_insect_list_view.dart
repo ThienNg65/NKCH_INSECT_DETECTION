7c28c31f85be3379da67f8523e50e762b670fabb
Thien Nguyen <nvtt1510@gmail.com>
2023-10-14 14:54:58 +0700

    Add gesturedetector

--- insect_dection_app/lib/features/insect/pages/widgets/similar_taxonomic_insect_list_view.dart
+++ insect_dection_app/lib/features/insect/pages/widgets/similar_taxonomic_insect_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../insect.dart';
+
 class SimilarTaxonomicInsectListView extends StatelessWidget {
   const SimilarTaxonomicInsectListView({
     super.key,
@@ -15,23 +17,11 @@ class SimilarTaxonomicInsectListView extends StatelessWidget {
       padding: const EdgeInsets.all(15.0),
       margin: const EdgeInsets.all(20.0),
       height: 170,
-      width: 400,
+      width: double.infinity,
       child: ListView(
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
         children: [
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
-          SimilarTaxonomicInsectListTile(),
           SimilarTaxonomicInsectListTile(),
           SimilarTaxonomicInsectListTile(),
           SimilarTaxonomicInsectListTile(),
@@ -43,27 +33,37 @@ class SimilarTaxonomicInsectListView extends StatelessWidget {
 }
 
 class SimilarTaxonomicInsectListTile extends StatelessWidget {
+  // final Insect insect;
+  final void Function()? onTap;
+  const SimilarTaxonomicInsectListTile({
+    super.key,
+    // required this.insect,
+    this.onTap,
+  });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10.0),
-      padding: const EdgeInsets.all(10.0),
-      child: const Column(
-        children: [
-          CircleAvatar(
-            backgroundColor: Colors.black,
-            radius: 35,
-            // backgroundImage: insect.photoUrl.isNotEmpty
-            //     ? NetworkImage(insect.photoUrl)
-            //     : null,
-          ),
-          SizedBox(height: 10),
-          Text(
-            "Naruto Na ",
-            overflow: TextOverflow.clip,
-            maxLines: 1,
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
+        child: const Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 35,
+              // backgroundImage: insect.photoUrl.isNotEmpty
+              //     ? NetworkImage(insect.photoUrl)
+              //     : null,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Hello",
+              overflow: TextOverflow.clip,
+              maxLines: 1,
+            ),
+          ],
+        ),
       ),
     );
   }
